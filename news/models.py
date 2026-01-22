19904baabb69a0aa3a8e3958b4b84a06104bb722
Abdubasitov <abdubasitovabai01@gmail.com>
2026-01-22 08:55:46 +0600

    django first project

---
+++ news/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
+
+
+class News(models.Model):
+	title = models.CharField(max_length=500)
+	image = models.ImageField(upload_to='newsMedia', null=True)
+	description = CKEditor5Field('Text', config_name='extends')
+
+	def __str__(self):
+		return self.title
+	
+	class Meta:
+		verbose_name_plural = 'Новости'
+		verbose_name = 'Новость'
+		ordering = ['-id']
