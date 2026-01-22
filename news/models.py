a7e303f260c721eb1bf44fcbd60df3afb5f80c7e
Abdubasitov <abdubasitovabai01@gmail.com>
2026-01-22 14:18:26 +0600

    update admin

--- news/models.py
+++ news/models.py
@@ -4,7 +4,7 @@ from django_ckeditor_5.fields import CKEditor5Field
 
 class News(models.Model):
 	title = models.CharField(max_length=500)
-	image = models.ImageField(upload_to='newsMedia', null=True)
+	image = models.ImageField(upload_to='newsMedia', null=True, blank=True)
 	description = CKEditor5Field('Text', config_name='extends')
 
 	def __str__(self):
